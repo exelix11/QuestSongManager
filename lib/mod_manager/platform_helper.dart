92245709be50acbff0d91b1d71a6ec6928bd6e15
exelix11 <13405476+exelix11@users.noreply.github.com>
2024-05-25 07:48:26 -0700

    Fix open song path on Windows

--- lib/mod_manager/platform_helper.dart
+++ lib/mod_manager/platform_helper.dart
@@ -5,7 +5,7 @@ import 'package:bsaberquest/mod_manager/model/song.dart';
 class PlatformHelper {
   static void openSongPath(Song song) {
     if (Platform.isWindows) {
-      Process.run('explorer', [song.folderPath]);
+      Process.run('explorer', [song.folderPath.replaceAll('/', '\\')]);
     } else if (Platform.isLinux) {
       Process.run('xdg-open', [song.folderPath]);
     } else {
